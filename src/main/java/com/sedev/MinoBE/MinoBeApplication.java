ac631384f3b8364a055b404a6c531a88daacef91
Le Minh Bao <besilent308@gmail.com>
2024-03-04 17:11:45 +0700

    docs: Generate default project folder

---
+++ src/main/java/com/sedev/MinoBE/MinoBeApplication.java
@@ -0,0 +1,13 @@
+package com.sedev.MinoBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sedev/MinoBE/MinoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedev.MinoBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
